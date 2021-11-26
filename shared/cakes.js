85dec0b8679ea9556c207496834d3beb0c59eda0
Charmonson <77593573+Charmonson@users.noreply.github.com>
2021-11-26 08:46:21 -0600

    initial commit

---
+++ shared/cakes.js
@@ -0,0 +1,34 @@
+export const CAKES = [
+    {
+        id: 0,
+        name: "Cherry Cupcake",
+        image: "/assets/images/cherry-cupcake.jpg",
+        elevation: 1233,
+        featured: false,
+        description: "Delicious combination of dark chocolate & cherry."
+    },
+    {
+        id: 1,
+        name:"Birthday Cupcake ",
+        image: "/assets/images/bday-cupcake",
+        elevation: 877,
+        featured: false,
+        description: "Special treat for that special day."
+    },
+    {
+        id: 2,
+        name:"No Birthday Cupcake",
+        image: "/assets/images/breadcrumb-trail.jpg",
+        elevation: 2901,
+        featured: false,
+        description: "Red velvet goodness for no birthday days."
+    },
+    {
+        id: 3,
+        name:"Ferrero Rocher Chocolate",
+        image: "/assets/images/chocolate.jpg",
+        elevation: 42,
+        featured: true,
+        description: "Not such thing as too much chocolate."
+    }
+];
